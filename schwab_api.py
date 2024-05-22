94935012fd03bc7ef94f0372fec28b073a265b9a
LCuretti <curetti.luis@gmail.com>
2024-05-21 23:28:41 -0300

    push test

--- schwab_api.py
+++ schwab_api.py
@@ -190,7 +190,6 @@ class schwabApi():
         # return teh resposnse of the get request.
         return requests.get(url=url, headers=merged_headers, verify = True, timeout = 10).json()
 
-
     #### ACCOUNT
 
     def get_account_numbers(self):
